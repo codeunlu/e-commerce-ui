51d7bcd3c3f8bf1b09948b5f050bae188fd97a85
busrapinar <bsrapnrr@gmail.com>
2024-02-08 23:30:18 +0300

    filterBox and sidebar

--- src/components/filterbox/FilterBox.tsx
+++ src/components/filterbox/FilterBox.tsx
@@ -1,31 +1,23 @@
-
+import React, { ReactNode } from 'react';
 import Button from '../button/Button';
-import FilterType from './FilterType';
 
+type FilterBoxProps = {
+  children: ReactNode; 
+  filterTitle: string;
+};
 
-
-const FilterBox = () => {
-    
-    return (
-        <div>
-            <h2 className='text-lg mb-2'>Sort By</h2>
-            <div className="border p-4 mb-4 rounded-md shadow-md">
-                <div className="flex flex-col gap-4 mb-2">
-                    <div className="flex flex-col">
-                        <FilterType filterType='Old to new' />
-                        <FilterType filterType='New to old' />
-                        <FilterType filterType='Price Low to High' />
-                        <FilterType filterType='Price High to Low' />
-                    </div>
-
-                    <Button addClass='bg-primary text-white'>
-                        Filtrele
-                    </Button>
-                </div>
-            </div>
+const FilterBox: React.FC<FilterBoxProps> = ({ children,filterTitle }) => {
+  return (
+    <div>
+      <h2 className="text-lg mb-2">{filterTitle}</h2>
+      <div className="border p-4 mb-4 rounded-md shadow-md">
+        <div className="flex flex-col gap-4 mb-2">
+          <div className="flex flex-col">{children}</div>
+          <Button addClass="bg-primary text-white">Filtrele</Button>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FilterBox;
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,13 +1,33 @@
 import FilterBox from "@/components/filterbox/FilterBox";
+import FilterType from "@/components/filterbox/FilterType";
 import React from "react";
 
 type Props = {};
 
 const SideBar = (props: Props) => {
+  return (
+    <div className="w-[300px] h-screen">
+      <FilterBox filterTitle="Sort by">
+        <FilterType filterType="Old to new" />
+        <FilterType filterType="New to old" />
+        <FilterType filterType="Price Low to High" />
+        <FilterType filterType="Price High to Low" />
+      </FilterBox>
 
-  return <div className="w-[300px] h-screen">
-    <FilterBox/>
-  </div>;
+      <FilterBox filterTitle="Brands">
+        <FilterType filterType="Apple" />
+        <FilterType filterType="Samsung" />
+        <FilterType filterType="Huawei" />
+      </FilterBox>
+
+      <FilterBox filterTitle="Model">
+        <FilterType filterType="11" />
+        <FilterType filterType="12 Pro" />
+        <FilterType filterType="13" />
+        <FilterType filterType="13 Pro Max" />
+      </FilterBox>
+    </div>
+  );
 };
 
 export default SideBar;
