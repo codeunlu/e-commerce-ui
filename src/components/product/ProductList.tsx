a33aae91a9136d79b986dbe757d3abcc3e485e3d
busrapinar <bsrapnrr@gmail.com>
2024-02-08 23:17:20 +0300

    Loading and NoRecordsMessage component

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,6 +1,8 @@
 import { Product } from "@/utils/type";
 import ProductCard from "./ProductCard";
 import Pagination from "../shared/Pagination";
+import Loading from "../shared/Loading";
+import NoRecordsMessage from "../shared/NoRecordsMessahe";
 
 type Props = {
   products: Product[];
@@ -24,13 +26,14 @@ export const ProductList = ({
   return (
     <div className="flex flex-col gap-y-6 mb-6">
       <div className="grid grid-cols-3 gap-4">
-        {loading && <div>loading</div>}
+        {loading && <Loading />}
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-        {!products.length && !loading && <div>Kayıtlar Çekilemedi</div>}
+        {!products.length && !loading && <NoRecordsMessage />}
       </div>
+      {!loading && (
       <Pagination
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
@@ -38,6 +41,7 @@ export const ProductList = ({
         onPrev={onPrev}
         onNext={onNext}
       />
+      )}
     </div>
   );
 };
---
+++ src/components/shared/NoRecordsMessahe.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoRecordsMessage: React.FC = () => {
+  return <div className="flex justify-center items-center text-lg ">Kayıtlar Çekilemedi</div>;
+};
+
+export default NoRecordsMessage;
