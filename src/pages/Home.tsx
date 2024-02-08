518775b3c905da36625286105020d8646808f4a4
Mustafa UNLU <codeunlu@gmail.com>
2024-02-08 17:59:59 +0300

    updated call api.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,29 @@
+import useCallApi from "@/hooks/useCallApi";
 import { Sidebar, Rightbar } from "@layouts/index";
+import { useEffect } from "react";
+
+type Product = {
+  createdAt: string;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  model: string;
+  brand: string;
+  id: string;
+};
 
 const Home = () => {
+  const getProducts = useCallApi<Product>("products");
+
+  useEffect(() => {
+    getProducts.call("GET", {
+      onSuccess: (data: Product) => {
+        console.log("data", data);
+      },
+    });
+  }, []);
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-around gap-2">
