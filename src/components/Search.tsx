5db7c6ce647fc6a3d0f5b58da0ad5c20f705bd94
Mustafa UNLU <codeunlu@gmail.com>
2024-02-08 17:39:03 +0300

    created user & search component.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,12 +6,10 @@ type Props = {
 
 const Search = ({ placeholder }: Props) => {
   return (
-    <>
-      <div className="flex gap-2 items-center justify-center border bg-white rounded-lg p-1">
-        <FaSearch className="w-6 h-6 text-gray-500 cursor-pointer" />
-        <input type="text" placeholder={placeholder} className="h-6" />
-      </div>
-    </>
+    <div className="flex gap-2 items-center justify-center border bg-white rounded-lg p-1">
+      <FaSearch className="w-6 h-6 text-gray-500 cursor-pointer" />
+      <input type="text" placeholder={placeholder} className="h-6" />
+    </div>
   );
 };
 
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,12 +2,10 @@ import { FaUser } from "react-icons/fa";
 
 const User = () => {
   return (
-    <>
-      <div className="flex gap-2">
-        <FaUser className="w-6 h-6" />
-        <p className="text-lg font-medium">Kerem</p>
-      </div>
-    </>
+    <div className="flex gap-2">
+      <FaUser className="w-6 h-6" />
+      <p className="text-lg font-medium">Kerem</p>
+    </div>
   );
 };
 
