a81bb2d58b736df7bf7eba2e8dc4b1d528d6944b
busrapinar <bsrapnrr@gmail.com>
2024-02-22 16:29:38 +0300

    hidden overflow in ShoppingCart component

--- src/components/shopping-wallet/ShoppingCart.tsx
+++ src/components/shopping-wallet/ShoppingCart.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const ShoppingCart = ({ children, totalPrice }: Props) => {
   return (
-    <div className="flex flex-col gap-8 px-2">
-      <div className=" w-full h-auto shadow-md rounded-md">
+    <div className="flex flex-col gap-8 px-2 ">
+      <div className=" w-auto max-h-[350px] overflow-y-auto shadow-md rounded-md">
         <div className="flex flex-col gap-y-4">{children}</div>
       </div>
       <div className="flex flex-col gap-4 w-full h-auto px-2 py-4 border shadow-lg rounded-md">
